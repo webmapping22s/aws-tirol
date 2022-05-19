7f08cead3d434958336e5f29849374e48a3ceeb8
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-05-19 16:03:29 +0200

    Temperaturlayer beim Laden anzeigen

--- main.js
+++ main.js
@@ -52,8 +52,8 @@ L.control.scale({
 // Fullscreen control
 L.control.fullscreen().addTo(map);
 
-// Wetterstationslayer beim Laden anzeigen
-overlays.stations.addTo(map);
+// diese Layer beim Laden anzeigen
+overlays.temperature.addTo(map);
 
 // Wetterstationen mit Icons und Popups
 let drawStations = function(geojson) {
