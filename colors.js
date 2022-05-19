cadfb223059f3383c30b11e032a40192e1c120e1
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-05-19 18:13:19 +0200

    Farbpalette Wind hinzugefügt

--- colors.js
+++ colors.js
@@ -89,5 +89,35 @@ const COLORS = {
         min: 400,
         max: 15000,
         color: "#cd0feb"
-    }]
+    }],
+    wind: [{
+        min: 0,
+        max: 5,
+        color: "#ffff64"
+    }, {
+        min: 5,
+        max: 10,
+        color: "#c8ff64"
+    }, {
+        min: 10,
+        max: 20,
+        color: "#96ff96"
+    }, {
+        min: 20,
+        max: 40,
+        color: "#32c8ff"
+    }, {
+        min: 40,
+        max: 60,
+        color: "#6496ff"
+    }, {
+        min: 60,
+        max: 80,
+        color: "#9664ff"
+    }, {
+        min: 80,
+        max: 300,
+        color: "#ff3232"
+    }      
+    ]
 }
