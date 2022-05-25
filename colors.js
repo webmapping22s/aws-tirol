4dd3aea6c1742f7d34316715084c227a6f32492f
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-05-25 16:06:16 +0200

    beautify für color.js

--- colors.js
+++ colors.js
@@ -1,91 +1,90 @@
 const COLORS = {
     temperature: [{
-            min: -50,
-            max: -25,
-            color: "#9f80ff"
-        }, {
-            min: -25,
-            max: -20,
-            color: "#784cff"
-        }, {
-            min: -20,
-            max: -15,
-            color: "#0f5abe"
-        }, {
-            min: -15,
-            max: -10,
-            color: "#1380ff"
-        }, {
-            min: -10,
-            max: -5,
-            color: "#19cdff"
-        }, {
-            min: -5,
-            max: -0,
-            color: "#8fffff"
-        }, {
-            min: -0,
-            max: 5,
-            color: "#b0ffbc"
-        }, {
-            min: 5,
-            max: 10,
-            color: "#ffff73"
-        }, {
-            min: 10,
-            max: 15,
-            color: "#ffbe7d"
-        }, {
-            min: 15,
-            max: 20,
-            color: "#ff9b41"
-        }, {
-            min: 20,
-            max: 25,
-            color: "#ff5a41"
-        }, {
-            min: 25,
-            max: 30,
-            color: "#ff1e23"
-        }, {
-            min: 30,
-            max: 50,
-            color: "#fa3c96"
-        }
-    ],
+        min: -50,
+        max: -25,
+        color: "#9f80ff"
+    }, {
+        min: -25,
+        max: -20,
+        color: "#784cff"
+    }, {
+        min: -20,
+        max: -15,
+        color: "#0f5abe"
+    }, {
+        min: -15,
+        max: -10,
+        color: "#1380ff"
+    }, {
+        min: -10,
+        max: -5,
+        color: "#19cdff"
+    }, {
+        min: -5,
+        max: -0,
+        color: "#8fffff"
+    }, {
+        min: -0,
+        max: 5,
+        color: "#b0ffbc"
+    }, {
+        min: 5,
+        max: 10,
+        color: "#ffff73"
+    }, {
+        min: 10,
+        max: 15,
+        color: "#ffbe7d"
+    }, {
+        min: 15,
+        max: 20,
+        color: "#ff9b41"
+    }, {
+        min: 20,
+        max: 25,
+        color: "#ff5a41"
+    }, {
+        min: 25,
+        max: 30,
+        color: "#ff1e23"
+    }, {
+        min: 30,
+        max: 50,
+        color: "#fa3c96"
+    }],
     snowheight: [{
         min: 0,
         max: 1,
         color: "#fff"
-    },{
+    }, {
         min: 1,
         max: 10,
         color: "#ffffb2"
-    },{
+    }, {
         min: 10,
         max: 25,
         color: "#b0ffbc"
-    },{
+    }, {
         min: 25,
         max: 50,
         color: "#8cffff"
-    },{
+    }, {
         min: 50,
         max: 100,
         color: "#19cdff"
-    },{
+    }, {
         min: 100,
         max: 200,
         color: "#1982ff"
-    },{
+    }, {
         min: 200,
         max: 300,
         color: "#0f5abe"
-    },{
+    }, {
         min: 300,
         max: 400,
         color: "#784bff"
-    },{
+    }, {
         min: 400,
         max: 15000,
         color: "#cd0feb"
@@ -118,6 +117,5 @@ const COLORS = {
         min: 80,
         max: 300,
         color: "#ff3232"
-    }      
-    ]
+    }]
 }
